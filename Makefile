2f57f2c2e4e2e0226110b7bca9dff259a3df6e97
Flederossi <oskar.web.thieme@gmail.com>
2023-10-31 14:09:56 +0100

    Added simple repl

--- Makefile
+++ Makefile
@@ -2,9 +2,10 @@ CFLAGS ?= -Wall -Wextra -std=c99
 
 example: eme.h
 	$(CC) example.c -o example -lm $(CFLAGS)
-
-run: example
 	./example
-
-clean:
 	rm example
+
+repl: eme.h
+	$(CC) repl.c -o repl -lm $(CFLAGS)
+	./repl
+	rm repl
---
+++ repl.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "eme.h"
+
+int main(void){
+	int err;
+	double res;
+	char in[1024];
+
+	printf("\n");
+	for (;;){
+		printf("> ");
+		fgets(in, sizeof(in) * sizeof(char), stdin);
+		res = eme_eval(in, &err);
+		if (err < 0) printf("= INVALID");
+		else printf("= %f", res);
+		printf("\n\n");
+	}
+
+	return 0;
+}
